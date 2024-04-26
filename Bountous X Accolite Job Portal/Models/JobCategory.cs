d0ba682696b47e651ce350dee3ba051881d6de8f
ShagunPandey2 <shagun.pandey@accolitedigital.com>
2024-04-26 10:59:03 +0530

    locationupdate

---
+++ Bountous X Accolite Job Portal/Models/JobCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Bountous_X_Accolite_Job_Portal.Models
+{
+    public class JobCategory
+    {
+        public Guid CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
