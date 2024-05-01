158f43ced8cf2f66796b14db9dad3941411d046e
Vishal Kumar Sahu (INT1587) <vishal.sahu@accolitedigital.com>
2024-05-01 10:12:53 +0530

    Adding jwtl

--- Bountous X Accolite Job Portal/obj/Debug/net8.0/Bountous X Accolite Job Portal.AssemblyInfo.cs
+++ Bountous X Accolite Job Portal/obj/Debug/net8.0/Bountous X Accolite Job Portal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c90bcbca1cf4c56d3dccce5f9bcce80b0ace8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08c849980bc7bb2b9d8054af25d8c717f76302d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
