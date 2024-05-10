f569daa5127fce474e8b75bb68d801d70a940225
Ankit Garg <ankit.garg@accolitedigital.com>
2024-05-10 13:44:31 +0530

    New Branch FIles

--- Bountous X Accolite Job Portal/obj/Debug/net8.0/Bountous X Accolite Job Portal.AssemblyInfo.cs
+++ Bountous X Accolite Job Portal/obj/Debug/net8.0/Bountous X Accolite Job Portal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a16308ddb7dabf1b849dc9acc4c1bb9b97188d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9794e8a603e7ea91963cc7f3f1d12c331b66c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
