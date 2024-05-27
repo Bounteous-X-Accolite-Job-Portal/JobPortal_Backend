e484f30dfcf45da43068e7764285e977e4f15f52
Vishal Kumar Sahu (INT1587) <vishal.sahu@accolitedigital.com>
2024-05-27 14:33:09 +0530

    Solving redis timeout error

--- Bountous X Accolite Job Portal/obj/Debug/net8.0/Bountous X Accolite Job Portal.AssemblyInfo.cs
+++ Bountous X Accolite Job Portal/obj/Debug/net8.0/Bountous X Accolite Job Portal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8b4feaec3bcd21af17132f4f223b14fc98da9e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c9a2e0a3c016e6d97320073dc3c9a0b5d52b7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
