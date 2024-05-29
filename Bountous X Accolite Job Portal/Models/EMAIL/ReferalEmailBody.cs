b660afe02b065b9bcd3b2098701216912f144114
ankit-accolite <162313535+ankit-accolite@users.noreply.github.com>
2024-05-29 13:04:57 +0530

    Update ReferalEmailBody.cs

--- Bountous X Accolite Job Portal/Models/EMAIL/ReferalEmailBody.cs
+++ Bountous X Accolite Job Portal/Models/EMAIL/ReferalEmailBody.cs
@@ -20,7 +20,7 @@ This referral is a testament to your skills and qualifications, and we are thril
 
 <h5>To take advantage of this referral and explore job opportunities with us, please follow these simple steps:</h5>
 <ol>
-<li>Visit the bounteous x Accolite Job Portal login page at http://localhost:4200/login .</li>
+<li>Visit the bounteous x Accolite Job Portal login page at https://kind-dune-058eee70f.5.azurestaticapps.net/login .</li>
 <li>Login to your account using the password {AutoPassword}</li>
 <li>Once logged in, navigate to the ""Job Listings"" section to view our current job openings.</li>
 <li>Browse through the available positions and select the ones that align with your skills, experience, and career goals.</li>
@@ -30,7 +30,7 @@ This referral is a testament to your skills and qualifications, and we are thril
 <br>
 We encourage you to explore our job listings thoroughly and apply to the roles that best match your qualifications. Your application will receive special attention as a referred candidate, and we look forward to considering you for suitable opportunities within our organization.
 <br>
-If you have any questions or need assistance with the application process, please don't hesitate to reach out to our support team at supportaccolite@gmail.com. We're here to help you every step of the way.
+If you have any questions or need assistance with the application process, please don't hesitate to reach out to our support team at support@accolitedigital.com. We're here to help you every step of the way.
 <br>
 Thank you for considering a career with bounteous x Accolite. We appreciate your interest and look forward to the possibility of welcoming you to our team.
 <br>
