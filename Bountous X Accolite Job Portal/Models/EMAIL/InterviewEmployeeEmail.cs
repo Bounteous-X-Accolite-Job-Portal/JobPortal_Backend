e7e7b171cf81a745164cb16460c2e91a54df20cf
Ankit Garg <ankit.garg@accolitedigital.com>
2024-05-30 09:07:04 +0530

    New Upgraded

---
+++ Bountous X Accolite Job Portal/Models/EMAIL/InterviewEmployeeEmail.cs
@@ -0,0 +1,35 @@
+﻿namespace Bountous_X_Accolite_Job_Portal.Models.EMAIL
+{
+    public class InterviewEmployeeEmail
+    {
+        public static string EmailStringBody(string name,string candidateName , string link, DateOnly? InterviewDate, TimeOnly? InterviewTime)
+        {
+            return $@"<html>
+<head>
+</head>
+<body>
+<h1>Interview Scheduled</h1>
+
+
+<h5>Dear Employer {name},</h5>
+
+<p>Interview has been Scheduled with Candidate {candidateName}:- </p>
+<p>Interview Date : {InterviewDate}</p>
+<p>Interview Time : {InterviewTime}</p>
+<p>Interview Link : {link}</p>
+
+Regards,<br>
+Team bounteuous x Accolite Job Portal
+
+</body>
+</html>
+
+
+
+
+";
+
+
+        }
+    }
+}
