cabd64758a4107986c85e4cc117940f7aa6c66a7
Ankit Garg <ankit.garg@accolitedigital.com>
2024-06-13 16:12:53 +0530

    Disable Account Email Added

--- Bountous X Accolite Job Portal/obj/Debug/net8.0/Bountous X Accolite Job Portal.AssemblyInfo.cs
+++ Bountous X Accolite Job Portal/obj/Debug/net8.0/Bountous X Accolite Job Portal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c186d2db535f642181cbec522ddc18cef8ed7a2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a173235f91d0db628e9b1f23bf75f1d4b914adc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bountous X Accolite Job Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
